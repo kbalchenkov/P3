eeee1c57365238026587474e01b36bdcc95b2894
Kirill Balchenkov <kirill00k@gmail.com>
2014-07-17 00:21:46 -0400

    Added lorem ipsum and Random users Package

--- app/views/Lorem-Ipsum.blade.php
+++ app/views/Lorem-Ipsum.blade.php
@@ -15,7 +15,18 @@
     <h1>Welcome to Lorem Ipsum Generator</h1
 	
 		
+		How many paragraphs do you want?
 	
+	<form method='POST'>
+	
+		<input name="_token" type="hidden" value="1KMXZmIJL14jqewAJ2Z5eyCjUpaTXu2oWYooDDPn">	
+		<label for="paragraphs">Paragraphs</label>				
+		<input maxlength="2" name="paragraphs" type="text" value="5" id="paragraphs"> (Max: 99)
+		
+		<br><br>
+			
+		<input type="submit" value="Generate!">    
+    </form>
 
 @stop
 
